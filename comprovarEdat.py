bb20e5d929a89720fa2d7c18daf040b44eff56bc
Izan <izan.arnaiz.7e5@itb.cat>
2023-09-18 21:36:28 +0200

    Primer commit

---
+++ comprovarEdat.py
@@ -0,0 +1,19 @@
+"""
+Izan Arnaiz Gallego
+17/09/2023, 18/09/2023
+ASIXc M03 UF1 A1
+Descripció:
+Estructura d'un programa informàtic - Algoritmes - Git
+"""
+
+# Este ejercicio está hecho para saber si una persona es mayor o menor de edad.
+
+comprovarEdat = int(input("¿Que edad tienes? "))
+
+if comprovarEdat >= 18:
+    print("Eres mayor de edad.")
+    print("... Programa finalizado ...")
+else:
+    print("Eres menor de edad.")
+    print("... Programa finalizado ...")
+
---
+++ hello.py
@@ -0,0 +1,9 @@
+"""
+Izan Arnaiz Gallego
+17/09/2023, 18/09/2023
+ASIXc M03 UF1 A1
+Descripció:
+Estructura d'un programa informàtic - Algoritmes - Git
+"""
+
+print("Hola món ;-) ")
